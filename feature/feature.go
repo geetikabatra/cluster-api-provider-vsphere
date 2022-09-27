458dc70d14088edaba26247569521cc7d991954d
Sagar Muchhal <muchhals@vmware.com>
2022-09-27 00:03:50 -0700

    Adds feature flag for node labeling
    Signed-off-by: Sagar Muchhal <muchhals@vmware.com>

--- feature/feature.go
+++ feature/feature.go
@@ -22,12 +22,20 @@ import (
 )
 
 const (
-// Every capv-specific feature gate should add method here following this template:
-//
-// // owner: @username
-// // alpha: v1.X
-// MyFeature featuregate.Feature = "MyFeature".
+	// Every capv-specific feature gate should add method here following this template:
+	//
+	// // owner: @username
+	// // alpha: v1.X
+	// MyFeature featuregate.Feature = "MyFeature".
 
+	// NodeLabeling is a feature gate for the functionality to propagate Machine labels
+	// with the prefix to the Node objects.
+	// This is a stop-gap measure which will be removed when we have this functionality
+	// present in CAPI.
+	// See https://github.com/kubernetes-sigs/cluster-api/pull/6255
+	//
+	// alpha: v1.5
+	NodeLabeling featuregate.Feature = "NodeLabeling"
 )
 
 func init() {
@@ -38,5 +46,5 @@ func init() {
 // To add a new feature, define a key for it above and add it here.
 var defaultCAPVFeatureGates = map[featuregate.Feature]featuregate.FeatureSpec{
 	// Every feature should be initiated here:
-
+	NodeLabeling: {Default: false, PreRelease: featuregate.Alpha},
 }
